3b02bd53a69fa4ce475b64b51389ccedc9eb26e9
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-02 18:27:39 +0000

    02-10-20-27

--- pages/Screeny_inventory/jsobjects/htmlgit/htmlgit.js
+++ pages/Screeny_inventory/jsobjects/htmlgit/htmlgit.js
@@ -2,7 +2,7 @@ export default {
   generateHTML() {
     let html = "szerokosc wysokosc"; // Start table and add headers
     tabela_screeny_inventory_id.tableData.forEach(row => {
-      html += `${row.szerokosc} <span> ${row.wysokosc}`; // Add rows with data
+      html += `<table><td>${row.szerokosc} </td><span> ${row.wysokosc}</table>`; // Add rows with data
     });
     html += ""; // Close table
     return html;
