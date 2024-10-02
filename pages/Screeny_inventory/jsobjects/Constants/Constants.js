cd2a71f100303cb45b3b09e97895dd849208f8c7
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-02 21:16:47 +0000

    02-10-2024

---
+++ pages/Screeny_inventory/jsobjects/Constants/Constants.js
@@ -0,0 +1,39 @@
+export default {
+	defaultLanguageKeys: {
+		"User Management": "User Management",
+		"Filter By": "Filter By",
+		"Find User": "Find User",
+		"Favorite Columns": "Favorite Columns",
+		"Select Favorite Columns": "Select Favorite Columns",
+		"Close": "Close",
+		"Bulk Update": "Bulk Update",
+		"Language": "Language",
+		"avatar": "Avatar",
+		"name": "Name",
+		"email": "Email",
+		"gender": "Gender",
+		"address": "Address",
+		"status": "Status",
+		"createdAt": "Created At",
+		"updatedAt": "Updated At",
+		"Confirm": "Confirm",
+		"Clear": "Clear",
+		"Delete": "Delete",
+		"Switching Languages": "Switching Languages"
+	},
+	languageOptions: [
+		"English",
+		"Polish",
+		"French",
+		"German",
+		"Dutch",
+		"Chinese",
+		"Japanese",
+		"Spanish",
+		"Arabic",
+		"Hindi",
+		"Russian",
+		"Portugese",
+		"Urdu"
+	]
+}
---
+++ pages/Screeny_inventory/jsobjects/i18n/i18n.js
@@ -0,0 +1,35 @@
+export default {
+	resources: {
+		english: {
+			translation: Constants.defaultLanguageKeys
+		}
+	},
+	getResources() {
+		return this.resources;
+	},
+	async selectLanguage(lang = Select1.selectedOptionValue){
+		if (!this.resources[lang]){
+			showModal(LanguageLoading.name);
+			await getLanguageValuesAI.run();
+			closeModal(LanguageLoading.name);
+			this.resources[lang] = {
+				translation: JSON.parse(getLanguageValuesAI.data.response)
+			};
+			this.setup(lang);
+		} else {
+			i18next.changeLanguage(lang);
+			this.translate();
+		}
+	},
+	async setup(lang='english'){
+		await i18next.init({
+			debug: true,
+			resources: this.resources
+		});
+		await i18next.changeLanguage(lang);
+		this.translate();
+	},
+	translate(key){
+		return i18next.t(key)
+	}
+}
