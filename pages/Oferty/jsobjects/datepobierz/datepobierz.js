f1d42a91d9a7a6e7684e65e18b4b9592ada7b398
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-08 18:08:49 +0000

    08-10-20-08

--- pages/Oferty/jsobjects/datepobierz/datepobierz.js
+++ pages/Oferty/jsobjects/datepobierz/datepobierz.js
@@ -11,7 +11,7 @@ var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-today = mm + '.' + dd + '.' + yyyy;
+today = dd + '.' + mm + '.' + yyyy;
 return (today);
 	}
 }
