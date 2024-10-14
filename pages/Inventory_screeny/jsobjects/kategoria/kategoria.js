66ef95b679fb8d0cb457e107f1c6f6d640372835
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-14 16:11:28 +0000

    14-10-18-11

--- pages/Inventory_screeny/jsobjects/kategoria/kategoria.js
+++ pages/Inventory_screeny/jsobjects/kategoria/kategoria.js
@@ -5,12 +5,27 @@ var wybranakategoria = kategoriaselect.selectedOptionLabel;
 //var skrzynka = Select8.selectedOptionValue;
 //var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
 if (wybranakategoria === 'SCREENY') { 
-uruchom = linia_zapytaj.run().then(() => {
-  system_zapytaj.run();
-  kolor_zapytaj.run();
-});
-}
-else  uruchom = {}; 
-return uruchom;
+  showModal(LanguageLoading.name)
+	system_zapytaj.run()
+  closeModal(LanguageLoading.name);
+};
+
 },
+	
+//async kategoria2(kategoria3 = kategoriaselect.selectedOptionValue){
+//		if (!this.resources[SCREENY]){
+//			showModal(LanguageLoading.name);
+//			await system_zapytaj.run();
+//			closeModal(LanguageLoading.name);
+				
+			
+//		} else {
+			
+//		}
+//	},
+
+	
+	
+	
+	
 }
