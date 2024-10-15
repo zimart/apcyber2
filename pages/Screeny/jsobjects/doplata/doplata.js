4af9d59b7fede8c54e12ed078e24ed60bb9bf6f6
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-15 20:49:02 +0000

    zepsute_screeny

--- pages/Screeny/jsobjects/doplata/doplata.js
+++ pages/Screeny/jsobjects/doplata/doplata.js
@@ -4,14 +4,14 @@ kolor: function () {
 //var wartoscdoplaty = doplatakolorinput.text;
 var cenasystemubaza = cena_systemu.text;
 //var skrzynka = Select8.selectedOptionValue;
-var doplatakolor = (cenasystemubaza*(doplatakolorinput.text/100));
+var doplatakolor = (cenasystemubaza*(doplatakolorinput22.text/100));
 //if (szerokoscm === '1') { szerm33 = podana; }
 //else if (szerokoscm === '2') { szerm33 = podana ; }
 return doplatakolor.toFixed(2);
 },
 	
 tkanina: function () {
-var kosztmetra = doplatatkaninam2.text;
+var kosztmetra = cena_doplata_lunar_m2.text;
 var powierzchniam2 = appsmith.store.powierzchniast;
 //var skrzynka = Select8.selectedOptionValue;
 var doplatatkanina = (doplatatkaninam2.text * powierzchnia.text);
--- pages/Screeny/jsobjects/ladowanie_2/ladowanie_2.js
+++
@@ -1,9 +0,0 @@
-export default {
- showModalOnPageLoad() {
-                   showModal(LanguageLoading.name)
-	system_zapytaj.run()
-	.then(() => 
-  
-closeModal(LanguageLoading.name));
-                 }
-}
