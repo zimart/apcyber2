d7f5c3f707942ea74e8660d9ed736efeb070087b
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-20 15:28:44 +0000

    20-10-17-28

--- pages/Podsumowanie/jsobjects/kurswaluta/kurswaluta.js
+++ pages/Podsumowanie/jsobjects/kurswaluta/kurswaluta.js
@@ -14,6 +14,15 @@ var kursik = 1;
 if (wybranawaluta === "PLN") { kursik = 1; }
 else if (wybranawaluta === "EUR") {  kursik = kurseuro.text ; }
 return kursik;
+},
+	
+kurs_dodatkowe: function () {
+var wybranawaluta = Select6.selectedOptionValue;
+var kursikeuro = kursinput.text;
+if (wybranawaluta === "PLN") { kwotapln = ""; }
+else if (wybranawaluta === "EUR") {  kwotapln = kursikeuro.text * Input2.text ; }
+return kwotapln;
 }
+	
 	
 }
