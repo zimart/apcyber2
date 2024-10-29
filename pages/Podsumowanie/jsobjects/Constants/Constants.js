d708bfa071eba678d940295e5ea74dd0337d7e0f
Artur Zimoń <a.zimon@sigma.sanok.pl>
2024-10-29 20:30:35 +0000

    29-10-21-30

--- pages/Podsumowanie/jsobjects/Constants/Constants.js
+++ pages/Podsumowanie/jsobjects/Constants/Constants.js
@@ -22,18 +22,8 @@ export default {
 		"Brutto":"Brutto",
 		"Podatek VAT":"Podatek VAT",
 		"Montaż":"Montaż",
-		"Pilot 1-kanałowy ręczny":"Pilot 1-kanałowy ręczny",
-		"Do uzgodnienia,":"Do uzgodnienia,",
-		"Wymagana kaucja w wysokości wykazanej poniżej przy przyjęciu zamówienia,":"Wymagana kaucja w wysokości wykazanej poniżej przy przyjęciu zamówienia,",
-		"Wymagany zadatek w wysokości wykazanej poniżej przy przyjęciu zamówienia,":"Wymagany zadatek w wysokości wykazanej poniżej przy przyjęciu zamówienia,",
-		"Pozostała wartość zamówienia płatna po montażu,":"Pozostała wartość zamówienia płatna po montażu,",
-		"Montaż płatny po montażu,":"Montaż płatny po montażu,",
-		"Przedpłata 100% wartości towaru,":"Przedpłata 100% wartości towaru,",
-		"Przedpłata 100% wartości zamówienia razem z montażem,":"Przedpłata 100% wartości zamówienia razem z montażem,",
-		"Przedpłata 100% wartości towaru przed dostawą,":"Przedpłata 100% wartości towaru przed dostawą,",
-		"Pozostała wartość towaru przed dostawą,":"Pozostała wartość towaru przed dostawą,",
-		"KREDYT KUPIECKI - TERMIN":"KREDYT KUPIECKI - TERMIN",
-		"Płatność po zrealizowaniu zamówienia,":"Płatność po zrealizowaniu zamówienia,"
+		"Pilot 1-kanałowy ręczny":"Pilot 1-kanałowy ręczny"
+	
 				
 	},
 	languageOptions: [
